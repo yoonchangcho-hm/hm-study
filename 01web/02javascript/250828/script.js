7c35a895a7ec8e4a114efdbcd5a4e0161dc3b485
yoonchang <yoonchangcho@gmail.com>
2025-09-03 15:01:38 +0900

    first

---
+++ 01web/02javascript/250828/script.js
@@ -0,0 +1,14 @@
+// alert('test');
+console.log('접속완료');
+
+function clickBtn(obj, money, name) {
+    console.log('클릭완료');
+    console.log(obj.src);
+    console.log(money);
+    console.log(name);
+    obj.src = 'travel2.jpg';
+}
+// document.write('<h3>test</h3>');
+// console.log('test');
+// console.log('ddd');
+// function fn() {}
