a06c5aafbfd349594d6024321c9c862bbc8eb528
yoonchangcho-hm <yoonchangcho@gmail.com>
2025-09-05 15:21:31 +0900

    250905_1520

---
+++ 02java/first_java/src/MyTest.java
@@ -0,0 +1,6 @@
+public class MyTest {
+    public static void main(String[] args) {
+        long num = 12345678900L;
+        System.out.println("hello java!!");
+    }
+}
--- 02java/first_java/src/Test.java
+++ 02java/first_java/src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("안녕하세요!!!");
+        int num = 10;
+        System.out.println("안녕하세요!!!" + num + "입니다.");
     }    
 }
