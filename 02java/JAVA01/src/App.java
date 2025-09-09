f2e1b1f6c8be9f25d40a5fc507b70fa1960ca31d
yoonchangcho-hm <yoonchangcho@gmail.com>
2025-09-09 15:26:27 +0900

    250909_1526

--- 02java/JAVA01/src/App.java
+++ 02java/JAVA01/src/App.java
@@ -1,5 +1,33 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // 정수 ->실수
+        // 실수 -> 정수
+
+        // 정수형 : long(8) > int(4) > short(2) > byte(1)
+        // 실수형 : double(8) > float(4)
+
+        // long * int = long
+        // double * int = double
+
+        int num = 10;
+        float floatNum = num;
+        System.out.println(floatNum);   // 묵시적
+        System.out.println((int)floatNum);  // 명시적
+
+
+        short sNum = 200;
+        System.out.println(sNum); // 200
+        byte byteNum = (byte)sNum;
+        System.out.println(byteNum); // -56
+
+        char myChar = 'A';
+        System.out.println((int)myChar);
+
+        char myChar1 = (char)42;
+        System.out.println(myChar)
+  
+
     }
 }
---
+++ 02java/JAVA01/src/test01/Scanner01.java
@@ -0,0 +1,23 @@
+package test01;
+
+import java.util.Scanner;
+
+public class Scanner01 { 
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);  
+        System.out.println("문자열을 입력하세요");scan6
+        
+        // String strInput = scan.nextLine();
+        // String strInput1 = scan.nextLine();
+        // System.out.println("입력된 값 :" + strInput + ", " + strInput1);
+        // scan.close();
+
+        String strInput1 = scan.nextLine();
+        int num1 = Integer.parseInt(strInput1);
+        int num2 = 5;
+        System.out.println(num1 + num2);
+    }
+}
+    
