cb1ff6cd5c1abdea90808f9bec797f7364ef3019
yoonchangcho-hm <yoonchangcho@gmail.com>
2025-09-09 16:42:16 +0900

    250909_1640

--- 02java/JAVA01/src/test01/FuncTest01.java
+++ 02java/JAVA01/src/test01/FuncTest01.java
@@ -5,11 +5,26 @@ public class FuncTest01 {
     public static void sayHello(String name, int year) {
             System.out.println(name + "님, 안녕하세요!" + year);
     }
+
+    public static int add(int a,int b){
+        return a + b;
+    }
+
+    public static String text(String a){
+        return a;
+    }
     public static void main(String[] args) {
         sayHello("홍길동",21);
         sayHello("김유신",21);
         sayHello("이순신",21);
         sayHello("철수",21);
         sayHello("홍길동",21);
+    
+        int result = add(10,20);
+        System.out.println("합은 : " + result);
+
+        String result1 = text("반갑습니다.");
+        System.out.println(result1);
+    
     }
 }
